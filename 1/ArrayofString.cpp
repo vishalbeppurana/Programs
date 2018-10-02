86331cd23e8efdbe0ad606ff892d8978ad6e6b3a
vishalbeppurana <vishalbep6@gmail.com>
2018-10-02 12:30:44 +0530

    Included array of String

---
+++ 1/ArrayofString.cpp
@@ -0,0 +1,25 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//    char arr[7][10] ={"Red","Green","Blue","Yellow","Pink","white","Purple"};
+//    for(int i=0;i<7;i++)
+//    {
+//        cout<<arr[i]<<endl;
+//    }
+//    return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//    int x[3][4]={{0,1,2,3},{4,5,6,7},{8,9,10,11}};
+//    for(int i=0;i<3;i++)
+//    {
+//        for(int j=0;j<4;j++)
+//        {
+//            cout<<"Elements at ["<<i<<"]"<<"["<<j<<"]"<<x[i][j]<<endl;
+//        }
+//    }
+//    return 0;
+//}
